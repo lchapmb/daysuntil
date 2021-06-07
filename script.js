588c82dd9329487d793cf6d58e0839f81f593c69
lchapmb <lchapmb@googlemail.com>
2021-06-07 13:18:37 +0100

    updated variable names, now const, todays date extracted out

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-$("#trigger").click(function(){
-  var apples = new Date();
-  var oranges = new Date(2021,12,25)
-  var pears = parseInt((oranges-apples)/(24*3600*1000));
-  
-  $("#days-until").append("<li>" + pears.toString()+" days until Christmas</li>");
-})
+const todaysDate = new Date();
 
-$("#trigger").click(function(){
-  var apples = new Date();
-  var oranges = new Date(2022,01,01)
-  var pears = parseInt((oranges-apples)/(24*3600*1000));
-  
-  $("#days-until").append("<li>" + pears.toString()+" days until New Year</li>");
-})
+$('#trigger').click(function () {
+  const christmas = new Date(2021, 12, 25);
+  const daysRemaining = parseInt((christmas - todaysDate) / (24 * 3600 * 1000));
 
-$("#trigger").click(function(){
-  var apples = new Date();
-  var oranges = new Date(2022,02,14)
-  var pears = parseInt((oranges-apples)/(24*3600*1000));
-  
-  $("#days-until").append("<li>" + pears.toString()+" days until Valentines Day</li>");
-})
+  $('#days-until').append(
+    '<li>' + daysRemaining.toString() + ' days until Christmas</li>'
+  );
+});
+
+$('#trigger').click(function () {
+  const newYears = new Date(2022, 01, 01);
+  const daysRemaining = parseInt((newYears - todaysDate) / (24 * 3600 * 1000));
+
+  $('#days-until').append(
+    '<li>' + daysRemaining.toString() + ' days until New Year</li>'
+  );
+});
+
+$('#trigger').click(function () {
+  const valentines = new Date(2022, 02, 14);
+  const daysRemaining = parseInt(
+    (valentines - todaysDate) / (24 * 3600 * 1000)
+  );
+
+  $('#days-until').append(
+    '<li>' + daysRemaining.toString() + ' days until Valentines Day</li>'
+  );
+});
