7437b4c3caee733401b5a6502f7e4ed938faf56b
lchapmb <lchapmb@googlemail.com>
2021-06-07 13:43:32 +0100

    reduces number of click functions, now contains all instances of getDifference being called

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const todaysDate = new Date();
 const christmas = new Date(2021, 12, 25);
-const newYears = new Date(2022, 01, 01);
+const newYear = new Date(2022, 01, 01);
 const valentines = new Date(2022, 02, 14);
 
 // takes the holiday and subtracts todays date, returning parsed integer
@@ -8,7 +8,7 @@ const daysRemaining = (holiday) => {
   return parseInt((holiday - todaysDate) / (24 * 3600 * 1000));
 };
 
-//take the holiday date and a string representing the holiday name, returning a li element with days remaining from todays date until the holiday
+// takes the holiday date and a string representing the holiday name, returning a li element with days remaining from todays date until the holiday
 const getDifference = (holiday, holName) => {
   return `<li> ${daysRemaining(
     holiday
@@ -17,18 +17,6 @@ const getDifference = (holiday, holName) => {
 
 $('#trigger').click(function () {
   $('#days-until').append(getDifference(christmas, 'Christmas'));
-});
-
-$('#trigger').click(function () {
-  $('#days-until').append(
-    '<li>' + daysRemaining(newYears).toString() + ' days until New Year</li>'
-  );
-});
-
-$('#trigger').click(function () {
-  $('#days-until').append(
-    '<li>' +
-      daysRemaining(valentines).toString() +
-      ' days until Valentines Day</li>'
-  );
+  $('#days-until').append(getDifference(newYear, 'New Year'));
+  $('#days-until').append(getDifference(valentines, 'Valentines Day'));
 });
