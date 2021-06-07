aedc1568cd6d5246b16c631e603198e3a1da930a
lchapmb <lchapmb@googlemail.com>
2021-06-07 12:43:51 +0100

    initial commit after copying over code from codepen

---
+++ index.js
@@ -0,0 +1,29 @@
+$('#trigger').click(function () {
+  var apples = new Date();
+  var oranges = new Date(2021, 12, 25);
+  var pears = parseInt((oranges - apples) / (24 * 3600 * 1000));
+
+  $('#days-until').append(
+    '<li>' + pears.toString() + ' days until Christmas</li>'
+  );
+});
+
+$('#trigger').click(function () {
+  var apples = new Date();
+  var oranges = new Date(2022, 01, 01);
+  var pears = parseInt((oranges - apples) / (24 * 3600 * 1000));
+
+  $('#days-until').append(
+    '<li>' + pears.toString() + ' days until New Year</li>'
+  );
+});
+
+$('#trigger').click(function () {
+  var apples = new Date();
+  var oranges = new Date(2022, 02, 14);
+  var pears = parseInt((oranges - apples) / (24 * 3600 * 1000));
+
+  $('#days-until').append(
+    '<li>' + pears.toString() + ' days until Valentines Day</li>'
+  );
+});
