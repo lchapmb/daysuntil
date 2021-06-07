32c18e4200f9d9c1126b1bc653a4c0a6897e5226
lchapmb <lchapmb@googlemail.com>
2021-06-07 14:09:03 +0100

    changes comment explaining daysRemaining function

--- script.js
+++ script.js
@@ -3,16 +3,14 @@ const christmas = new Date(2021, 11, 25);
 const newYear = new Date(2022, 00, 01);
 const valentines = new Date(2022, 01, 14);
 
-// takes the holiday and subtracts todays date, returning parsed integer
+// takes the holiday and subtracts todays date, returning the result as a number rounded up to the next integer
 const daysRemaining = (holiday) => {
   return Math.ceil((holiday - todaysDate) / (24 * 3600 * 1000));
 };
 
 // takes the holiday date and a string representing the holiday name, returning an li element with days remaining from todays date until the holiday
 const getDifference = (holiday, holName) => {
-  return `<li> ${daysRemaining(
-    holiday
-  ).toString()} days until ${holName} </li>`;
+  return `<li> ${daysRemaining(holiday)} days until ${holName} </li>`;
 };
 
 $('#trigger').click(function () {
