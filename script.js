5cfcae2be286938278872942df383a2b0a5d1411
lchapmb <lchapmb@googlemail.com>
2021-06-07 13:39:19 +0100

    creates getDifference function to return an li element

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const todaysDate = new Date();
 
+// takes the holiday and subtracts todays date, returning parsed integer
 const daysRemaining = (holiday) => {
   return parseInt((holiday - todaysDate) / (24 * 3600 * 1000));
 };
 
+//take the holiday date and a string representing the holiday name, returning a li element with days remaining from todays date until the holiday
+const getDifference = (holiday, holName) => {
+  return `<li> ${daysRemaining(
+    holiday
+  ).toString()} days until ${holName} </li>`;
+};
+
 $('#trigger').click(function () {
   const christmas = new Date(2021, 12, 25);
-  $('#days-until').append(
-    '<li>' + daysRemaining(christmas).toString() + ' days until Christmas</li>'
-  );
+  $('#days-until').append(getDifference(christmas, 'Christmas'));
 });
 
 $('#trigger').click(function () {
