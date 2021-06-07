5a929967a3a42b748a6e5674f79663351e6e2990
lchapmb <lchapmb@googlemail.com>
2021-06-07 14:04:29 +0100

    replaces parseInt with Math ceil in daysremaining function

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const todaysDate = new Date();
-const christmas = new Date(2021, 12, 25);
-const newYear = new Date(2022, 01, 01);
-const valentines = new Date(2022, 02, 14);
+const christmas = new Date(2021, 11, 25);
+const newYear = new Date(2022, 00, 01);
+const valentines = new Date(2022, 01, 14);
 
 // takes the holiday and subtracts todays date, returning parsed integer
 const daysRemaining = (holiday) => {
-  return parseInt((holiday - todaysDate) / (24 * 3600 * 1000));
+  return Math.ceil((holiday - todaysDate) / (24 * 3600 * 1000));
 };
 
 // takes the holiday date and a string representing the holiday name, returning an li element with days remaining from todays date until the holiday
