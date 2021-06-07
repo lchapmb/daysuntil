c567960379e2d7dc56a5cc0c893b6956ba81c86d
lchapmb <lchapmb@googlemail.com>
2021-06-07 13:26:41 +0100

    creates daysRemaining function, incorporates this into li element

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 const todaysDate = new Date();
 
+const daysRemaining = (holiday) => {
+  return parseInt((holiday - todaysDate) / (24 * 3600 * 1000));
+};
+
 $('#trigger').click(function () {
   const christmas = new Date(2021, 12, 25);
-  const daysRemaining = parseInt((christmas - todaysDate) / (24 * 3600 * 1000));
-
   $('#days-until').append(
-    '<li>' + daysRemaining.toString() + ' days until Christmas</li>'
+    '<li>' + daysRemaining(christmas).toString() + ' days until Christmas</li>'
   );
 });
 
 $('#trigger').click(function () {
   const newYears = new Date(2022, 01, 01);
-  const daysRemaining = parseInt((newYears - todaysDate) / (24 * 3600 * 1000));
-
   $('#days-until').append(
-    '<li>' + daysRemaining.toString() + ' days until New Year</li>'
+    '<li>' + daysRemaining(newYears).toString() + ' days until New Year</li>'
   );
 });
 
 $('#trigger').click(function () {
   const valentines = new Date(2022, 02, 14);
-  const daysRemaining = parseInt(
-    (valentines - todaysDate) / (24 * 3600 * 1000)
-  );
-
   $('#days-until').append(
-    '<li>' + daysRemaining.toString() + ' days until Valentines Day</li>'
+    '<li>' +
+      daysRemaining(valentines).toString() +
+      ' days until Valentines Day</li>'
   );
 });
